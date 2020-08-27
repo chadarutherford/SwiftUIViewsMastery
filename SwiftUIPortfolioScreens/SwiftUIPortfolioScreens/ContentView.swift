8ed1f24e77d4293aff110e91af111c81066f317c
Chad Rutherford <chad_rutherford@outlook.com>
2020-08-27 12:19:41 -0400

    Added Portfolio Screens Project

---
+++ SwiftUIPortfolioScreens/SwiftUIPortfolioScreens/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUIPortfolioScreens
+//
+//  Created by Chad Rutherford on 8/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
