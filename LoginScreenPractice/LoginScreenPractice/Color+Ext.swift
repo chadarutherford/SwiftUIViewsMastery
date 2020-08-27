6591c0111fd128e21297e00699d10ec766032632
Chad Rutherford <chad_rutherford@outlook.com>
2020-08-26 20:58:51 -0400

    Finished Adding ColorScheme to application

---
+++ LoginScreenPractice/LoginScreenPractice/Color+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Ext.swift
+//  LoginScreenPractice
+//
+//  Created by Chad Rutherford on 8/26/20.
+//
+
+import SwiftUI
+
+extension Color {
+	static let themeBackground = Color("BackgroundColor")
+	static let themeAccentColor = Color("ThemeAccentColor")
+	static let themeForeground = Color("ForegroundColor")
+	static let themeSecondary = Color("SecondaryColor")
+	static let themeTertiary = Color("TertiaryColor")
+}
--- LoginScreenPractice/LoginScreenPractice/Login Screens/Login1.swift
+++ LoginScreenPractice/LoginScreenPractice/Login Screens/Login1.swift
@@ -13,59 +13,68 @@ struct Login1: View {
 	@State private var password = ""
 	
     var body: some View {
-		VStack(spacing: 20) {
-			Text("Login Screen 1")
-				.font(.largeTitle)
-				.fontWeight(.medium)
+		ZStack {
+			Color.themeBackground
+				.edgesIgnoringSafeArea(.all)
 			
-			HStack {
-				VStack {
-					Rectangle()
-						.frame(width: 30, height: 30)
-					Rectangle()
-						.frame(width: 30, height: 30)
+			VStack(spacing: 20) {
+				Text("Login Screen 1")
+					.foregroundColor(.themeForeground)
+					.font(.largeTitle)
+					.fontWeight(.medium)
+				
+				HStack {
+					VStack {
+						Rectangle()
+							.frame(width: 30, height: 30)
+						Rectangle()
+							.frame(width: 30, height: 30)
+					}
+					
+					VStack {
+						Rectangle()
+							.frame(width: 30, height: 30)
+						Rectangle()
+							.frame(width: 30, height: 30)
+					}
+				}
+				.rotationEffect(.degrees(45))
+				.foregroundColor(.themeSecondary)
+				
+				Spacer()
+				
+				TextField("Username:", text: $username)
+					.textFieldStyle(RoundedBorderTextFieldStyle())
+				
+				SecureField("Password:", text: $password)
+					.textFieldStyle(RoundedBorderTextFieldStyle())
+				
+				Spacer()
+				
+				Button(action: {}) {
+					Text("Button")
+						.foregroundColor(.white)
+						.padding()
+						.frame(width: 150)
+						.background(
+							Capsule()
+						)
 				}
 				
-				VStack {
-					Rectangle()
-						.frame(width: 30, height: 30)
-					Rectangle()
-						.frame(width: 30, height: 30)
+				Button(action: {}) {
+					Text("Forget password")
 				}
 			}
-			.rotationEffect(.degrees(45))
-			
-			Spacer()
-			
-			TextField("Username:", text: $username)
-				.textFieldStyle(RoundedBorderTextFieldStyle())
-			
-			SecureField("Password:", text: $password)
-				.textFieldStyle(RoundedBorderTextFieldStyle())
-			
-			Spacer()
-			
-			Button(action: {}) {
-				Text("Button")
-					.foregroundColor(.white)
-					.padding()
-					.frame(width: 150)
-					.background(
-						Capsule()
-					)
-			}
-			
-			Button(action: {}) {
-				Text("Forget password")
-			}
+			.padding(.horizontal)
+			.padding(.bottom)
 		}
-		.padding(.horizontal)
-		.padding(.bottom)
+		.accentColor(.themeAccentColor)
     }
 }
 
 struct Login1_Previews: PreviewProvider {
     static var previews: some View {
         Login1()
+			.preferredColorScheme(.dark)
     }
 }
